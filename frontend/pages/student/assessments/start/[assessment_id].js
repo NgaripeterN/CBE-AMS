0994f1512b0f123bf4825d7554105efb1316b398
NgaripeterN <ngarip254@gmail.com>
2025-12-09 13:20:03 +0300

    Fix: Cloudinary signed upload frontend API key issue

--- frontend/pages/student/assessments/start/[assessment_id].js
+++ frontend/pages/student/assessments/start/[assessment_id].js
@@ -150,7 +150,6 @@ const AssessmentSubmissionPage = () => {
       formData.append('folder', folder);
       formData.append('timestamp', timestamp);
       formData.append('signature', signature);
-      formData.append('api_key', process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY);
 
       const response = await axios.post(`https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/upload`, formData, {
         onUploadProgress: (progressEvent) => {
